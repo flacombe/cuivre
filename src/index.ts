ade2999978be613fd518ae04883d396bb371641c
FLacombe <francois.lacombe@infos-reseaux.com>
2024-07-28 22:04:59 +0200

    Fix git

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import maplibregl from 'maplibre-gl'
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-slider';
-import 'bootstrap-slider/dist/css/bootstrap-slider.min.css';
 
 import LayerSwitcher from '@russss/maplibregl-layer-switcher'
 import URLHash from '@russss/maplibregl-layer-switcher/urlhash'
