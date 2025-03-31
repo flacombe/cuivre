f3f9dfac8238f1c28b2461f5ffeef90aa23e89d0
FLacombe <francois.lacombe@infos-reseaux.com>
2025-03-31 12:27:05 +0200

    Fix fibredata script

--- db/fibredata.sql
+++ db/fibredata.sql
@@ -27,7 +27,7 @@ with data as (
     join cuivre_ftthimb fi on fi.fibre_id=fc.fibre_id
     join cuivre_ftthipe fp on fp.fibre_imb=fi.fibre_imb
 )
-INSERT INTO cuivre_fibre (fibre_id, fibre_imb, fibre_addr_num, fibre_addr_num_cp, fibre_addr_voie_type, fibre_addr_voie, fibre_addr_bat, fibre_insee, fibre_commune, fibre_dept, fibre_imb_cat, fibre_imb_etat, fibre_pm, fibre_pm_etat, fibre_l33, fibre_imb_type, fibre_point, fibre_point_3857, fibre_cuivre_ft_on, fibre_cuivre_fcr_on)
+INSERT INTO cuivre_fibre (fibre_id, fibre_imb, fibre_addr_num, fibre_addr_num_cp, fibre_addr_voie_type, fibre_addr_voie, fibre_addr_bat, fibre_insee, fibre_commune, fibre_dept, fibre_imb_cat, fibre_imb_etat, fibre_pm, fibre_pm_etat, fibre_l33, fibre_imb_type, fibre_point, fibre_point_3857, fibre_cuivre_fcr_on, fibre_cuivre_ft_on)
 SELECT d.*
 FROM data d;
 
